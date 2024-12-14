a8975068f5e6b39de5429811ec9abd69a6493e4e
Rafael <rafaelmoura7033@gmail.com>
2024-12-14 13:41:26 -0300

    Adding a button to reddirect to create page

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -3,12 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact list</title>
+    <title>Contact List</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-5">
         <h1 class="mb-4">Lista de Contatos</h1>
+        <div class="mb-4">
+            <a href="{{ route('contacts.create') }}" class="btn btn-primary">Adicionar Novo Contato</a>
+        </div>
+
         <table class="table table-bordered">
             <thead>
                 <tr>
